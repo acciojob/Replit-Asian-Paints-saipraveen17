bad77a3ecd3776653715119d7428217dc5ea7579
Praveen Katikireddy <praveenk9417@gmail.com>
2023-03-21 22:37:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+const gridItems = document.querySelectorAll(".grid-item");
+
+function resetColors() {
+  gridItems.forEach((item) => {
+    item.style.backgroundColor = "transparent";
+  });
+}
+
+function changeColor() {
+  const blockId = document.getElementById("block_id").value;
+  const color = document.getElementById("color_id").value;
+  
+  const block = document.getElementById(blockId);
+  if (block) {
+    resetColors();
+    block.style.backgroundColor = color;
+  } else {
+    alert("Invalid block ID!");
+  }
+}
+
+document.getElementById("change_button").addEventListener("click", changeColor);
+document.getElementById("reset_button").addEventListener("click", resetColors);
